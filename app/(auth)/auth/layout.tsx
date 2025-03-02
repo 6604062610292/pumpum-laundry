c42101ae463e58a1f4712f25ed58a99ead7520a9
Jozho <59310592+JozhoX@users.noreply.github.com>
2025-03-02 21:43:16 +0700

    Auth Implementation (#1)
    * init: declare session model to db schema
    
    * build: add @oslojs/encoding @oslojs/crypto
    
    * feat: add prisma singleton
    
    * feat: add auth controller
    
    * feat: add middleware
    
    * remove: boilerplate layout & page
    
    * feat: add auth layout
    
    * feat(auth): add session manager
    
    ---------
    
    Co-authored-by: zin <s6604062610292@email.kmutnb.ac.th>

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Prompt } from "next/font/google";
-import "./globals.css";
+import "../../globals.css";
+import { getCurrentSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 const prompt = Prompt({
   weight: "400",
@@ -11,11 +13,16 @@ export const metadata: Metadata = {
   title: "Pumpum Laundry",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { user } = await getCurrentSession();
+  if (user) {
+    return redirect("/");
+  }
+
   return (
     <html lang="en">
       <body className={`${prompt.className} antialiased`}>{children}</body>
