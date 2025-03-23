a34b442beb60ff33aa0f52b8e63706a950f6ca34
zin <s6604062610292@email.kmutnb.ac.th>
2025-03-23 21:09:04 +0700

    feat(zod.auth.schema): add surname field

--- lib/schema/auth.schema.ts
+++ lib/schema/auth.schema.ts
@@ -2,12 +2,14 @@ import { z } from "zod";
 
 const ZEmail = z.string().email();
 const ZName = z.string();
+const ZSurname = z.string();
 const ZPassword = z.string();
 const ZPhoneNumber = z.string().length(10);
 
 export const ZRegisterSchema = z.object({
   email: ZEmail,
   name: ZName,
+  surname: ZSurname,
   password: ZPassword,
   phone_number: ZPhoneNumber,
 });
