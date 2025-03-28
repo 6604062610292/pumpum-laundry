d97c0a0a5ece7d0eb0333b5370561a426f12bfc7
zin <s6604062610292@email.kmutnb.ac.th>
2025-03-29 04:06:13 +0700

    feat: add '/machines' endpoint

---
+++ app/api/v1/machines/route.ts
@@ -0,0 +1,46 @@
+import { prisma } from "@/lib/prisma";
+import { ZMachineSchema } from "@/lib/schema/machine.schema";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const formBody = await req.json();
+  const userBody = ZMachineSchema.safeParse(formBody);
+  if (!userBody.success) {
+    return NextResponse.json(
+      {
+        message: userBody.error.flatten(),
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    const { name } = userBody.data;
+    await prisma.machine.create({
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
