03df7e20449b92637462470eb9a5fe7eab7eac60
JozhoX <sippakorn1337@gmail.com>
2025-03-31 22:56:07 +0700

    feat(machines.api): add patch for controller

--- app/api/v1/machines/route.ts
+++ app/api/v1/machines/route.ts
@@ -1,5 +1,8 @@
 import { prisma } from "@/lib/prisma";
-import { ZMachineSchema } from "@/lib/schema/machine.schema";
+import {
+  ZMachineEditSchema,
+  ZMachineSchema,
+} from "@/lib/schema/machine.schema";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
@@ -50,13 +53,81 @@ export const GET = async () => {
     const numberOfMachine = await prisma.machine.count();
     return NextResponse.json(
       { success: true, count: numberOfMachine },
-      { status: 200 },
-    )
+      { status: 200 }
+    );
   } catch (err) {
     console.error(err);
     return NextResponse.json(
       { message: "Internal Server Error" },
-      { status: 500 },
-    )
+      { status: 500 }
+    );
   }
-}
+};
+
+export const PATCH = async (req: NextRequest) => {
+  const formBody = await req.json();
+  const userBody = ZMachineEditSchema.safeParse(formBody);
+  if (!userBody.success) {
+    return NextResponse.json(
+      {
+        message: userBody.error.flatten(),
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  try {
+    const { id, is_active, name } = userBody.data;
+    // Is machine valid?
+    const machine = await prisma.machine.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (!machine) {
+      return NextResponse.json(
+        {
+          message: "ไม่พบเครื่องดังกล่าว",
+        },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    // Update
+    await prisma.machine.update({
+      where: {
+        id: machine.id,
+      },
+      data: {
+        name,
+        is_active: Boolean(is_active),
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "อัพเดตข้อมูลเครื่องสำเร็จ",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      {
+        error: "Internal Server Error",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+};
