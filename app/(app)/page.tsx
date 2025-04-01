9b52752603ccad087fe132649b46a290ef33caa7
JozhoX <sippakorn1337@gmail.com>
2025-04-01 21:23:06 +0700

    remove: duplicate nav on index page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -68,12 +68,6 @@ export default async function Home() {
                   icon=<Users size={40} />
                   href="/admin/users"
                 />
-                {/* User list */}
-                <LargeNavigationButton
-                  label="รายชื่อผู้ใช้"
-                  icon=<Users size={40} />
-                  href="/admin/users"
-                />
               </>
             )}
           </div>
